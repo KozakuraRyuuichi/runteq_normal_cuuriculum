fecbfbc3d51e8fab32e6bebcb1785368ba59d272
KozakuraRyuuichi <k267280.352@gmail.com>
2022-04-20 21:21:43 +0900

    [modified]Userモデルの微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
   validates :last_name, length: { maximum: 255 }, if: -> { new_record? || changes[:crypted_password] }
   validates :first_name, length: { maximum: 255 }, if: -> { new_record? || changes[:crypted_password] 
 
-
   validates :email, uniqueness: true
   validates :last_name, :first_name, :email, presence: true
-
 end
