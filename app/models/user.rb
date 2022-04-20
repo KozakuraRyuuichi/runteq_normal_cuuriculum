998195dc9da215801315199378d9d5d872c22f31
KozakuraRyuuichi <k267280.352@gmail.com>
2022-04-20 21:34:17 +0900

    [modified]Userモデルのタイポ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :last_name, length: { maximum: 255 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :first_name, length: { maximum: 255 }, if: -> { new_record? || changes[:crypted_password] 
+  validates :first_name, length: { maximum: 255 }, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
   validates :last_name, :first_name, :email, presence: true
