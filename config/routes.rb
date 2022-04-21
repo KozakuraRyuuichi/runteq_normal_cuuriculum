fa66255061f95c83d6b710c2b3c89dd9a2488061
KozakuraRyuuichi <k267280.352@gmail.com>
2022-04-22 08:20:31 +0900

    [add]ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
+
+  get '/login', to:'user_sessions#new'
+  post '/login', to:'user_sessions#create'
+  delete '/logout', to:'user_sessions#destroy' 
+  
+  resources :users, only: %i[new create]
+
 end
