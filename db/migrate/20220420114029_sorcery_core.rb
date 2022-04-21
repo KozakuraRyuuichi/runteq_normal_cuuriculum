4c3c11143e0d64cd31ffb733e90ad807694f73f7
KozakuraRyuuichi <k267280.352@gmail.com>
2022-04-22 08:19:13 +0900

    [add]dbにカラムを追加

--- db/migrate/20220420114029_sorcery_core.rb
+++ db/migrate/20220420114029_sorcery_core.rb
@@ -4,6 +4,8 @@ class SorceryCore < ActiveRecord::Migration[5.2]
       t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
+      t.string :last_name,        null: false
+      t.string :first_name,       null: false
 
       t.timestamps                null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2022_04_20_114029) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
-        t.string "last_name", null: false
+    t.string "last_name", null: false
     t.string "first_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
