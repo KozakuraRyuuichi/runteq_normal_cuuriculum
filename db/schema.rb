4dd4f9e3b20b038bfae134a53b5bb1b8efb11547
KozakuraRyuuichi <k267280.352@gmail.com>
2022-06-13 20:31:14 +0900

    [add]dbの編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_024923) do
+ActiveRecord::Schema.define(version: 2022_06_13_112423) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_04_27_024923) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
